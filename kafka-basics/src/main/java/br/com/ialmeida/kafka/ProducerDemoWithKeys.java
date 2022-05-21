01da654e909ee47bea29143beafa64d6c75e9bef
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-05-21 06:45:11 -0300

    ProducerDemoWithKeys

---
+++ kafka-basics/src/main/java/br/com/ialmeida/kafka/ProducerDemoWithKeys.java
@@ -0,0 +1,72 @@
+package br.com.ialmeida.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithKeys {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProducerDemoWithKeys.class.getSimpleName());
+    public static final String HOST = "127.0.0.1:9092";
+
+    public static void main(String[] args) {
+        LOG.info("I am a Kafka Producer!");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, HOST);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // testing StickyPartitioner (sending data really fast)
+        for (int i = 0; i < 10; i++) {
+
+            String topic = "demo_java";
+            String value = "Hello, World!";
+            String key = "id_" + i;
+
+            // create a Producer record
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, key, value);
+
+            // send data - asynchronous
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (exception == null) {
+                        // the record was successfully sent
+                        LOG.info("Received new metadata \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Key: " + producerRecord.key() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        LOG.error("Error while producing!", exception);
+                    }
+                }
+            });
+
+            // forcing kafka not to go too fast (messages will change partitions)
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        // flush data - synchronous
+        producer.flush();
+
+        // flush and close the Producer (close method calls flush method already)
+        producer.close();
+
+    }
+
+}
